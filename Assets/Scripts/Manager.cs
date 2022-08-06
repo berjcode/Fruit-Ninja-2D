86512247a1240c4a0167942d5a507bc7f618abf7
Abdullah Balıkçı <abdblk2525@gmail.com>
2022-08-06 19:43:52 +0300

    Update
    Pratice

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Manager : MonoBehaviour
+{
+    public int score;
+    public Text scoreText;
+    private void Update()
+    {
+        gameScore();
+
+    }
+    private void gameScore()
+    {
+        scoreText.text= score.ToString();
+    }
+
+
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
